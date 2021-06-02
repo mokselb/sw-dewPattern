bc86a38a320f5c983251ca38cd38e28195c053be
mokselb1488 <bat-chat@ukr.net>
2021-06-02 15:11:27 +0300

    fix1

--- Singleton/Singleton/Source.cpp
+++ Singleton/Singleton/Source.cpp
@@ -5,45 +5,51 @@ using namespace std;
 class Singleton
 {
 private:
-	static Singleton* _object;
-	float length;
-	string name;
-	int age;
+	static Singleton* _morph;
+	int strength;
+	int agility;
+	int intelligence;
 protected:
 	Singleton()
 	{
-		length = 0;
-		name = string();
-		age = 0;
+		strength = 22;
+		agility = 0;
+		intelligence = 15;
 	}
 public:
-	static Singleton* Object()
+	static Singleton* Morph()
 	{
-		if (_object == NULL)
+		if (_morph == NULL)
 		{
-			_object = new Singleton;
+			_morph = new Singleton;
 		}
-		return _object;
+		return _morph;
 	}
-	float GetLength() { return length; }
-	string GetName() { return name; }
-	int GetAge() { return age; }
-	void SetLength(float _length) { length = _length; }
-	void SetName(string _name) { name = _name; }
-	void SetAge(int _age) { age = _age; }
-	~Singleton() { delete _object; }
+	int GetStrength() { return strength; }
+	int GetAgility() { return agility; }
+	int GetIntelligence() { return intelligence; }
+	void SetStrength(int _strength) { strength = _strength; }
+	void SetAgility(int _agility) { agility = _agility; }
+	void SetIntelligence(int _intelligence) { intelligence = _intelligence; }
+	~Singleton() { delete _morph; }
 };
-Singleton* Singleton::_object = NULL;
+Singleton* Singleton::_morph = NULL;
 int main()
 {
-	Singleton* Morphling = Singleton::Object();
-	Morphling->SetName("Morphling");
-	Morphling->SetAge(300);
-	Morphling->SetLength(1.6);
-	cout << "Name1 : " << Morphling->GetName() << endl;
-	Singleton* Pudge = Singleton::Object();
-	cout << "Name2.1 : " << Pudge->GetName() << endl;
-	Pudge->SetName("Pudge");
-	cout << "Neme2.2 : " << Pudge->GetName() << endl;
+	Singleton* Morphling = Singleton::Morph();//Some DotA mechanics are allowed for the pattern implementation
+	cout << "Morphling`s Strength : " << Morphling->GetStrength() << endl;
+	Morphling->SetAgility(24);
+	cout << "Morphling`s Agility : " << Morphling->GetAgility() << endl;
+	cout << "Morphling`s Intelligence : " << Morphling->GetIntelligence() << endl;
+	cout << "Morphling`s pointer : " << Morphling << endl;
+	Singleton* Pudge = Singleton::Morph();//Morphling changes its shape to the image and likeness of the selected enemy
+	Pudge->SetAgility(14);
+	Pudge->SetStrength(25);
+	Pudge->SetIntelligence(16);
+	cout << "Pudge`s pointer : " << Pudge << endl;//That`s steel Morphling by "ID", but looks and has properties seems to Pudge (or any another character)
+	//I'm just trying to implement the mechanics of one ultimate
+	cout << "Pudge`s Strength : " << Pudge->GetStrength() << endl;
+	cout << "Pudge`s Agility : " << Pudge->GetAgility() << endl;
+	cout << "Pudge`s Intelligence : " << Pudge->GetIntelligence() << endl;
 	return 0;
 }
--- Singleton/UnitTestSingleton/UnitTestSingleton.cpp
+++ Singleton/UnitTestSingleton/UnitTestSingleton.cpp
@@ -11,48 +11,25 @@ namespace UnitTestSingleton
 		
 		TEST_METHOD(TestMethod1)
 		{
-			Singleton* Morphling = Singleton::Object();
-			Morphling->SetName("Morphling");
-			Morphling->SetAge(300);
-			Morphling->SetLength(1.6);
-			string name = "Morphling";
-			float Length = 1.6;
-			Assert::AreEqual(Morphling->GetAge(), 300);
-			Assert::AreEqual(Morphling->GetName(), name);
-			Assert::AreEqual(Morphling->GetLength(), Length);
+			Singleton* Morphling = Singleton::Morph();
+			Morphling->SetAgility(24);
+			Assert::AreEqual(Morphling->GetStrength(), 22);
+			Assert::AreEqual(Morphling->GetAgility(), 24);
+			Assert::AreEqual(Morphling->GetIntelligence(), 15);
 		}
 		TEST_METHOD(TestMethod2)
 		{
-			Singleton* Morphling = Singleton::Object();
-			Morphling->SetName("Morphling");
-			string name = "Morphling";
-			Assert::AreEqual(Morphling->GetName(), name);
-			Singleton* Pudge = Singleton::Object();
-			Assert::AreEqual(Pudge->GetName(), name);
-			string name1 = "Pudge";
-			Pudge->SetName("Pudge");
-			Assert::AreEqual(Pudge->GetName(), name1);
-		}
-		TEST_METHOD(TestMethod3)
-		{
-			Singleton* Morphling = Singleton::Object();
-			Morphling->SetName("Morphling");
-			Morphling->SetAge(300);
-			Morphling->SetLength(1.6);
-			string name = "Morphling";
-			float Length = 1.6;
-			Singleton* Pudge = Singleton::Object();
-			Assert::AreEqual(Pudge->GetAge(), 300);
-			Assert::AreEqual(Pudge->GetName(), name);
-			Assert::AreEqual(Pudge->GetLength(), Length);
-			Pudge->SetName("Pudge");
-			Pudge->SetAge(159);
-			Pudge->SetLength(1.9);
-			string name1 = "Pudge";
-			float Length1 = 1.9;
-			Assert::AreEqual(Pudge->GetAge(), 159);
-			Assert::AreEqual(Pudge->GetName(), name1);
-			Assert::AreEqual(Pudge->GetLength(), Length1);
+			Singleton* Morphling = Singleton::Morph();
+			Morphling->SetAgility(24);
+			Singleton* Pudge = Singleton::Morph();
+			Assert::AreEqual(Pudge->GetAgility(), 24);//checking if the "new" object stores the properties of the old one
+			Pudge->SetAgility(14);
+			Pudge->SetStrength(25);
+			Pudge->SetIntelligence(16);
+			Assert::AreEqual(Pudge->GetStrength(), 25);
+			Assert::AreEqual(Pudge->GetAgility(), 14);
+		    Assert::AreEqual(Pudge->GetIntelligence(), 16);
+			Assert::IsTrue(Pudge == Morphling);//checking for coincidence of pointers
 		}
 	};
 }
